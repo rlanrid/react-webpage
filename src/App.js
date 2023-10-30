f9b051ab56c0c1388cdae932a181b0dd318a0ff1
rlanrid <rladnrider77@gmail.com>
2023-10-30 12:01:53 +0900

    react-webPage 수업내용 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import './assets/css/reset.css'
+import './assets/css/style.css'
+
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import Slider from './components/Slider'
+import Image from './components/Image'
+import ImgText from './components/ImgText'
+import Card from './components/Card'
+import Banner from './components/Banner'
+import Text from './components/Text'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <>
+            <Header element="nexon" />
+            <Main >
+                <Slider element="nexon" />
+                <Image element="nexon section" title="포트폴리오가 실력이다." />
+                <ImgText element="section nexon" title="이미지 텍스트 유형" />
+                <Card element="section nexon gray" title="웹스토리보이 강의" />
+                <Banner element="nexon" title="배너 영역" />
+                <Text element="section nexon" title="스크립트를 익히는 방법" />
+            </Main>
+            <Footer element="nexon section gray" />
+        </>
+    )
 }
 
 export default App
